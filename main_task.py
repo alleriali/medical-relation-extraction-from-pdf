5e01dcdb390d547ab28c779b2d6569953fbec903
alleriali <alleriali27@gmail.com>
2020-05-05 14:44:23 +0200

    add code files

---
+++ main_task.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  2 17:40:16 2019
+
+@author: weetee
+"""
+
+from src.tasks.preprocessing_funcs import load_dataloaders
+from src.tasks.trainer import train_and_fit
+from src.tasks.infer import infer_from_trained
+import logging
+from argparse import ArgumentParser
+from src.tasks.visualization import Graph
+from src.tasks.pdf_to_txt import PdfToTxt
+from src.tasks.merge_relations import MergeRelation
+from src.tasks.graph_visualization import GraphVisualization
+import glob,os
+import pandas as pd
+'''
+This fine-tunes the BERT model on SemEval task 
+'''
+
+logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s', \
+                    datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
+logger = logging.getLogger('__file__')
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--train_data", type=str, default='./data/SemEval2010_task8_all_data/SemEval2010_task8_training/treat_cause_p_train.txt', \
+                        help="training data .txt file path")
+    parser.add_argument("--test_data", type=str, default='./data/SemEval2010_task8_all_data/SemEval2010_task8_testing_keys/treat_cause_p_test.txt', \
+                        help="test data .txt file path")
+    parser.add_argument("--use_pretrained_blanks", type=int, default=0, help="0: Don't use pre-trained blanks model, 1: use pre-trained blanks model")
+    parser.add_argument("--num_classes", type=int, default=4, help='number of relation classes')
+    parser.add_argument("--batch_size", type=int, default=16, help="Training batch size")
+    parser.add_argument("--gradient_acc_steps", type=int, default=1, help="No. of steps of gradient accumulation")
+    parser.add_argument("--max_norm", type=float, default=1.0, help="Clipped gradient norm")
+    parser.add_argument("--fp16", type=int, default=0, help="1: use mixed precision ; 0: use floating point 32") # mixed precision doesn't seem to train well
+    parser.add_argument("--num_epochs", type=int, default=35, help="No of epochs")
+    parser.add_argument("--lr", type=float, default=0.00005, help="learning rate")
+    parser.add_argument("--model_no", type=int, default=2, help='''Model ID: 0 - BERT\n
+                                                                            1 - ALBERT''')
+    
+    parser.add_argument("--train", type=int, default=1, help="0: Don't train, 1: train")
+    parser.add_argument("--infer", type=int, default=1, help="0: Don't infer, 1: Infer")
+    parser.add_argument("--freeze", type=int, default=1, help='''1: Freeze most layers until classifier layers\
+                                                                \n0: Don\'t freeze \
+                                                                (Probably best not to freeze if GPU memory is sufficient)''')
+    args = parser.parse_args()
+    
+    if args.train == 1:
+        net = train_and_fit(args)
+    print("aaaa")
+    if args.infer == 1:
+        inferer = infer_from_trained(args, detect_entities=True)
+        test = "The surprise [E1]visit[/E1] caused a [E2]frenzy[/E2] on the already chaotic trading floor."
+        #test = "Other <E1>items</E1> advertised on TV simply melt the chocolate on the <E2>stovetop</E2>."
+        pred = inferer.infer_sentence(test, detect_entities=False)
+        print("111")
+        print(pred)
+        test2 = "After eating the chicken, he developed a sore throat the next morning."
+        pred = inferer.infer_sentence(test2, detect_entities=True)
+        print("222")
+        print(pred)
+
+        test3 = "Duplication of this publication or parts thereof is permitted only under the provisions of the Copyright Law of the Publisher’s location, in its current version, and permission for use must always be obtained from Springer."
+        pred = inferer.infer_sentence(test3, detect_entities=True)
+        print("333")
+        print(pred)
+
+        print("444")
+        test4 = "moreover , uft has proved to be effective for inoperable advanced malignancies such as colorectal cancer, especially in combination with leucovorin or cisplatin."
+        pred = inferer.infer_sentence(test4, detect_entities=True)
+        print(pred)
+        print("444")
+        test4 = "cancer can not be treated by dienogest."
+        pred = inferer.infer_sentence(test4, detect_entities=True)
+        print(pred)
+        test5 = "In more severe cases with high fever or marked prostration , hospitalization may be needed with IV acyclovir , antibiotics , ﬂuids , and pain medications ."
+        pred = inferer.infer_sentence(test5, detect_entities=True)
+        print(pred)
+        test6 ="GIANT CELL TUMOR OF THE TENDON SHEATH A giant cell tumor of the tendon sheath is the most common tumor of the hand and presents with a ﬁrm enlarging nodule on the ﬁngers ."
+        pred = inferer.infer_sentence(test6, detect_entities=True)
+        print(pred)
+        sents_for_pretraining =[]
+        merged = MergeRelation()
+        # for file in glob.glob('./data/PDF1/*'):
+        #     if file.find('.pdf') != -1:
+        #         print(file)
+        #         file_name = os.path.basename(file)
+        #         graph = Graph(file_name)
+        #         Pdf2txt = PdfToTxt(file)
+        #         input_sents = Pdf2txt.get_processed_sents()
+        #         sents_for_pretraining.extend(input_sents)
+        #         for sent in input_sents:
+        #             pred = inferer.infer_sentence(sent,detect_entities=True)
+        #             if pred is not None:
+        #                 graph.add_edge(pred)
+        #         csv_path = file.replace('.pdf', '.csv')
+        #         graph.kg_df.to_csv(csv_path, header=True)
+        #         merged.get_graph(graph.kg_df)
+        for file in glob.glob('./data/PDF1/*'):
+            if file.find('.csv')!=-1:
+                kg_df = pd.read_csv(file)
+                merged.get_graph(kg_df)
+        merged.kg_df = merged.process_df(merged.kg_df)
+        # with open('./data/sents_for_pretraining.txt','w+') as f:
+        #     for sent in sents_for_pretraining:
+        #         f.write(sent+'\n'+'\n')
+        merged.kg_df.to_csv('./data/PDF1/DF_full1.csv', index=False)
+        merged.vote_relations()
+        # merged.get_final_graph()
+        # merged.show_graph()
+
+        merged.voted_by_pdfs.to_csv('./data/PDF1/DF_vote_by_pdf.csv',index=False)
+        merged.voted_by_sents.to_csv('./data/PDF1/DF_vote_by_sents.csv', index=False)
+        # GraphVisua = GraphVisualization(merged.G)
+
+        # print('55555')
+        # while True:
+        #     # sent = input("Type input sentence ('quit' or 'exit' to terminate):\n")
+        #     # if sent.lower() in ['quit', 'exit']:
+        #     #     break
+        #     # inferer.infer_sentence(sent, detect_entities=True)
+        #     relation_name = input("Type the the name of the realtion you are interested in('quit' or 'exit' to terminate):\n")
+        #     if relation_name.lower() not in ['quit', 'exit']:
+        #         GraphVisua.show_gragh_by_edge(relation_name)
+        #     else:
+        #         break
+        #     node = input("Type the node or nodes you are interested in, input nodes with comma to separate them('quit' or 'exit' to terminate):\n")
+        #     if node.find(',') != -1:
+        #         node = node.split(',')
+        #         node = list(node)
+        #         GraphVisua.node_adj_and_shown(node)
+        #     elif node.lower() in ['quit', 'exit']:
+        #         break
+        #     else:
+        #         GraphVisua.node_adj_and_shown(node)
